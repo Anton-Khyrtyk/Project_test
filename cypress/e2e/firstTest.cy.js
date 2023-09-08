abf61b68c54f6b04e1039349429c89fa66fcf950
Anton-Khyrtyk <anton.khytryk@gmail.com>
2023-09-08 16:36:08 +0300

    Actions lecture

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,47 +1,108 @@
 
 ///<reference types="Cypress" />
 
-it('SHOULD', ()=>{
+//type
+it('type', ()=>{
     cy.visit('https://next.privat24.ua/mobile?lang=en')
-    cy.get('[data-qa-node="amount"]')
+        .get('[data-qa-node="phone-number"]')
+        .type(112233344)
+})
+
+//focus
+it('focus', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="amount"]')
+        .focus()
+})
+
+it('blur', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="amount"]')
+        .focus()
+        .blur()
+})
+
+it('clear', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-node="amount"]')
+        .type(999)
+        .wait(2000)
         .clear()
-        .type(100)
-        .should('have.value',100)
-        .and('be.visible')
 })
 
-it('EXPECT', ()=>{
-    cy.visit('https://next.privat24.ua/mobile?lang=en');
-    cy.get('[data-qa-node="amount"]')
-        .clear()
-        .type(100).then( input =>{
-            expect(input).to.have.value('100')
-        })
+it('submit', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('form[method="post"]')
+        .submit()
 })
 
-it('check default value for Deposit', ()=>{
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.get('[data-qa-value="UAH"]')
-        .should('be.checked')
+it('click', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-value="manual"]')
+        .click()
 })
 
-it('check is visible Archive link', ()=>{
-    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
-    cy.contains('My deposits')
-        .trigger('mouseover')
-        .get('#archiveDeposits')
-        .should('be.visible')
+it('rightclick', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-value="manual"]')
+        .rightclick()
 })
 
-it('check is correct attr button', ()=>{
-    cy.visit('https://next.privat24.ua?lang=en')
-    cy.contains('Show cards')
-        .should('have.attr', 'type')
-        .and('match', /button/)
+// this test doesn't work
+// it('doubleclick', ()=>{
+//     cy.visit('https://developer.mozilla.org/en-US/docs/Web/API/element/dblclick_event')
+//         .contains('My Card')
+//         .dblclick()
+// })
+
+it('check', ()=>{
+    cy.visit('https://www.facebook.com/reg/?')
+        .contains('Повернутися на головну сторінку')
+        .click()
+        .get('[data-testid="open-registration-form-button"]')
+        .click()
+        .get('input[value="2"]')
+        .wait(2000)
+        .check()
 })
 
-it.only('check is correct URL', ()=>{
-    cy.visit('https://next.privat24.ua?lang=en')
-    cy.url()
-        .should('eq', 'https://next.privat24.ua/?lang=en')
+it('uncheck', ()=>{
+    cy.visit('https://en.privatbank.ua/')
+        .get('#switch-input')
+        .check({force: true})
+        .wait(2000)
+        .uncheck({force: true})
 })
+
+it('select', ()=>{
+    cy.visit('https://www.facebook.com/reg/?')
+        .contains('Повернутися на головну сторінку')
+        .click()
+        .get('[data-testid="open-registration-form-button"]')
+        .click()
+        .get('#month')
+        .select('фев')
+        .get('#day')
+        .select('12')
+        .get('#year')
+        .select('1991')
+})
+
+it('scrollIntoView', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+        .get('[data-qa-value="English"]')
+        .scrollIntoView()
+})
+
+it('scrollTo', ()=>{
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.scrollTo(0, 500)
+})
+
+// this test doesn't work
+// it.only('trigger', ()=>{
+//     cy.visit('https://next.privat24.ua/?lang=en')
+//         // .contains('Deposits')
+//         .get('[data-qa-node="menu"]')
+//         .trigger('mouseover')
+// })
